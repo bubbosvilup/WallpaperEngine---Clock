977ae4d0ce43786ce52c545b313c099c99f67721
Nicco <be_fr@DESKTOP-DKTME93.localdomain>
2025-02-25 05:07:25 +0100

    changed bg - palette - added setting - added date and amPm

---
+++ script.js
@@ -0,0 +1,32 @@
+const timeText = document.getElementById("time");
+const secondsText = document.getElementById("seconds");
+const amPmText = document.getElementById("amPm");
+const dateText = document.getElementById("date");
+const menuButton = document.getElementById("menuButton");
+const settingsMenu = document.getElementById("settingsMenu");
+
+function getTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const day = now.getDate().toString().padStart(2, "0");
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
+  const year = now.getFullYear();
+
+  const amPm = hours >= 12 ? "PM" : "AM";
+  const displayHours = (hours % 12 || 12).toString().padStart(2, "0");
+
+  timeText.innerHTML = `${displayHours}:${minutes}`;
+  secondsText.innerHTML = `${seconds}`;
+  amPmText.innerHTML = `${amPm}`;
+  dateText.innerHTML = `${day}/${month}/${year}`;
+}
+
+menuButton.addEventListener("click", () => {
+  settingsMenu.classList.toggle("visible");
+  settingsMenu.classList.toggle("hidden");
+});
+
+setInterval(getTime, 999);
+getTime();
